6437df8ab8722d69bce4f61cf65292c88d22ddaa
Monica Olejniczak <molejniczak@atlassian.com>
2024-05-01 22:53:00 +1000

    .

--- crates/core/src/config/partial_parcel_config.rs
+++ crates/core/src/config/partial_parcel_config.rs
@@ -1,3 +1,4 @@
+use derive_builder::Builder;
 use indexmap::IndexMap;
 use std::{collections::HashSet, rc::Rc};
 
@@ -7,7 +8,8 @@ use super::{parcel_config::PluginNode, parcel_rc::ParcelRcFile};
 ///
 /// This data structure is used to perform configuration merging, to eventually create a compelete ParcelConfig.
 ///
-#[derive(Debug, Default, PartialEq)]
+#[derive(Clone, Debug, Default, Builder, PartialEq)]
+#[builder(default)]
 pub struct PartialParcelConfig {
   pub bundler: Option<PluginNode>,
   pub compressors: IndexMap<String, Vec<PluginNode>>,
@@ -25,18 +27,14 @@ impl From<&ParcelRcFile> for PartialParcelConfig {
   fn from(parcel_rc: &ParcelRcFile) -> Self {
     let resolve_from = Rc::new(parcel_rc.path.display().to_string());
 
+    let to_entry = |package_name: &String| PluginNode {
+      package_name: String::from(package_name),
+      resolve_from: Rc::clone(&resolve_from),
+    };
+
     let to_vec = |maybe_plugins: Option<&Vec<String>>| {
       maybe_plugins
-        .map(|plugins| {
-          plugins
-            .iter()
-            .map(|package_name| PluginNode {
-              package_name: String::from(package_name),
-              // resolve_from: Rc::clone(&resolve_from),
-              resolve_from: resolve_from.clone(),
-            })
-            .collect()
-        })
+        .map(|plugins| plugins.iter().map(to_entry).collect())
         .unwrap_or(Vec::new())
     };
 
@@ -48,13 +46,7 @@ impl From<&ParcelRcFile> for PartialParcelConfig {
             .map(|(pattern, plugins)| {
               (
                 String::from(pattern),
-                plugins
-                  .iter()
-                  .map(|package_name| PluginNode {
-                    package_name: String::from(package_name),
-                    resolve_from: Rc::clone(&resolve_from),
-                  })
-                  .collect(),
+                plugins.iter().map(to_entry).collect(),
               )
             })
             .collect()
@@ -67,15 +59,7 @@ impl From<&ParcelRcFile> for PartialParcelConfig {
         .map(|plugins| {
           plugins
             .iter()
-            .map(|(pattern, package_name)| {
-              (
-                String::from(pattern),
-                PluginNode {
-                  package_name: String::from(package_name),
-                  resolve_from: Rc::clone(&resolve_from),
-                },
-              )
-            })
+            .map(|(pattern, package_name)| (String::from(pattern), to_entry(package_name)))
             .collect()
         })
         .unwrap_or(IndexMap::new())
@@ -105,62 +89,63 @@ impl From<&ParcelRcFile> for PartialParcelConfig {
 
 // TODO validate ...
 impl PartialParcelConfig {
-  fn merge_map(
-    from_map: IndexMap<String, PluginNode>,
-    to_map: IndexMap<String, PluginNode>,
-  ) -> IndexMap<String, PluginNode> {
-    if to_map.is_empty() {
-      return from_map;
+  fn merge_map<T: Clone>(
+    map: IndexMap<String, T>,
+    extend_map: IndexMap<String, T>,
+    merge: fn(map: T, extend_map: T) -> T,
+  ) -> IndexMap<String, T> {
+    if extend_map.is_empty() {
+      return map;
     }
 
-    if from_map.is_empty() {
-      return to_map;
+    if map.is_empty() {
+      return extend_map;
     }
 
-    // TODO
-    from_map
-  }
-
-  fn merge_maps(
-    from_map: IndexMap<String, Vec<PluginNode>>,
-    to_map: IndexMap<String, Vec<PluginNode>>,
-  ) -> IndexMap<String, Vec<PluginNode>> {
-    if to_map.is_empty() {
-      return from_map;
-    }
-
-    if from_map.is_empty() {
-      return to_map;
-    }
-
-    let mut map = IndexMap::new();
+    let mut merged_map = IndexMap::new();
     let mut used_patterns = HashSet::new();
 
     // Add the extension options first so they have higher precedence in the output glob map
-    for (pattern, from_pipelines) in to_map {
-      let to_pipelines = from_map.get(&pattern);
-      if to_pipelines.is_some() {
+    for (pattern, extend_pipelines) in extend_map {
+      let map_pipelines = map.get(&pattern);
+      if map_pipelines.is_some() {
         used_patterns.insert(pattern.clone());
-        map.insert(
+        merged_map.insert(
           pattern,
-          PartialParcelConfig::merge_pipelines(from_pipelines, to_pipelines.unwrap().clone()),
+          merge(map_pipelines.unwrap().clone(), extend_pipelines),
         );
+      } else {
+        merged_map.insert(pattern, extend_pipelines);
       }
     }
 
     // Add remaining pipelines
-    for (pattern, pipelines) in from_map {
+    for (pattern, value) in map {
       if !used_patterns.contains(&pattern) {
-        map.insert(String::from(pattern), pipelines);
+        merged_map.insert(String::from(pattern), value);
       }
     }
 
-    map
+    merged_map
+  }
+
+  fn merge_pipeline_map(
+    map: IndexMap<String, PluginNode>,
+    extend_map: IndexMap<String, PluginNode>,
+  ) -> IndexMap<String, PluginNode> {
+    PartialParcelConfig::merge_map(map, extend_map, |map, _extend_map| map)
+  }
+
+  fn merge_pipelines_map(
+    from_map: IndexMap<String, Vec<PluginNode>>,
+    extend_map: IndexMap<String, Vec<PluginNode>>,
+  ) -> IndexMap<String, Vec<PluginNode>> {
+    PartialParcelConfig::merge_map(from_map, extend_map, PartialParcelConfig::merge_pipelines)
   }
 
   fn merge_pipelines(
     from_pipelines: Vec<PluginNode>,
-    to_pipelines: Vec<PluginNode>,
+    extend_pipelines: Vec<PluginNode>,
   ) -> Vec<PluginNode> {
     let spread_index = from_pipelines
       .iter()
@@ -170,8 +155,8 @@ impl PartialParcelConfig {
       None => from_pipelines,
       Some(index) => [
         &from_pipelines[..index],
-        to_pipelines.as_slice(),
-        &from_pipelines[index..],
+        extend_pipelines.as_slice(),
+        &from_pipelines[(index + 1)..],
       ]
       .concat(),
     }
@@ -180,13 +165,19 @@ impl PartialParcelConfig {
   pub fn merge(from_config: PartialParcelConfig, extend_config: PartialParcelConfig) -> Self {
     PartialParcelConfig {
       bundler: from_config.bundler.or(extend_config.bundler),
-      compressors: PartialParcelConfig::merge_maps(
+      compressors: PartialParcelConfig::merge_pipelines_map(
         from_config.compressors,
         extend_config.compressors,
       ),
       namers: PartialParcelConfig::merge_pipelines(from_config.namers, extend_config.namers),
-      optimizers: PartialParcelConfig::merge_maps(from_config.optimizers, extend_config.optimizers),
-      packagers: PartialParcelConfig::merge_map(from_config.packagers, extend_config.packagers),
+      optimizers: PartialParcelConfig::merge_pipelines_map(
+        from_config.optimizers,
+        extend_config.optimizers,
+      ),
+      packagers: PartialParcelConfig::merge_pipeline_map(
+        from_config.packagers,
+        extend_config.packagers,
+      ),
       reporters: PartialParcelConfig::merge_pipelines(
         from_config.reporters,
         extend_config.reporters,
@@ -196,11 +187,299 @@ impl PartialParcelConfig {
         extend_config.resolvers,
       ),
       runtimes: PartialParcelConfig::merge_pipelines(from_config.runtimes, extend_config.runtimes),
-      transformers: PartialParcelConfig::merge_maps(
+      transformers: PartialParcelConfig::merge_pipelines_map(
         from_config.transformers,
         extend_config.transformers,
       ),
-      validators: PartialParcelConfig::merge_maps(from_config.validators, extend_config.validators),
+      validators: PartialParcelConfig::merge_pipelines_map(
+        from_config.validators,
+        extend_config.validators,
+      ),
+    }
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  mod merge {
+    use super::*;
+
+    mod bundler {
+      use super::*;
+
+      #[test]
+      fn uses_from_when_extend_missing() {
+        let from = PartialParcelConfigBuilder::default()
+          .bundler(Some(PluginNode {
+            package_name: String::from("a"),
+            resolve_from: Rc::new(String::from("/")),
+          }))
+          .build()
+          .unwrap();
+
+        let extend = PartialParcelConfig::default();
+        let expected = from.clone();
+
+        assert_eq!(PartialParcelConfig::merge(from, extend), expected);
+      }
+
+      #[test]
+      fn uses_extend_when_from_missing() {
+        let from = PartialParcelConfig::default();
+        let extend = PartialParcelConfigBuilder::default()
+          .bundler(Some(PluginNode {
+            package_name: String::from("a"),
+            resolve_from: Rc::new(String::from("/")),
+          }))
+          .build()
+          .unwrap();
+
+        let expected = extend.clone();
+
+        assert_eq!(PartialParcelConfig::merge(from, extend), expected);
+      }
+
+      #[test]
+      fn merges_using_from() {
+        let from = PartialParcelConfigBuilder::default()
+          .bundler(Some(PluginNode {
+            package_name: String::from("a"),
+            resolve_from: Rc::new(String::from("/")),
+          }))
+          .build()
+          .unwrap();
+
+        let extend = PartialParcelConfigBuilder::default()
+          .bundler(Some(PluginNode {
+            package_name: String::from("b"),
+            resolve_from: Rc::new(String::from("/")),
+          }))
+          .build()
+          .unwrap();
+
+        let expected = from.clone();
+
+        assert_eq!(PartialParcelConfig::merge(from, extend), expected);
+      }
+    }
+
+    // TODO parameterize tests
+    mod compressors {
+      use super::*;
+      use indexmap::indexmap;
+
+      #[test]
+      fn uses_from_when_extend_missing() {
+        let from = PartialParcelConfigBuilder::default()
+          .compressors(indexmap! {
+            String::from("*.js") => vec!(PluginNode {
+              package_name: String::from("a"),
+              resolve_from: Rc::new(String::from("/")),
+            })
+          })
+          .build()
+          .unwrap();
+
+        let extend = PartialParcelConfig::default();
+        let expected = from.clone();
+
+        assert_eq!(PartialParcelConfig::merge(from, extend), expected);
+      }
+
+      #[test]
+      fn uses_extend_when_from_missing() {
+        let from = PartialParcelConfig::default();
+        let extend = PartialParcelConfigBuilder::default()
+          .compressors(indexmap! {
+            String::from("*.js") => vec!(PluginNode {
+              package_name: String::from("a"),
+              resolve_from: Rc::new(String::from("/")),
+            })
+          })
+          .build()
+          .unwrap();
+
+        let expected = extend.clone();
+
+        assert_eq!(PartialParcelConfig::merge(from, extend), expected);
+      }
+
+      #[test]
+      fn merges_patterns() {
+        let from = PartialParcelConfigBuilder::default()
+          .compressors(indexmap! {
+            String::from("*.js") => vec!(PluginNode {
+              package_name: String::from("a"),
+              resolve_from: Rc::new(String::from("/")),
+            })
+          })
+          .build()
+          .unwrap();
+
+        let extend = PartialParcelConfigBuilder::default()
+          .compressors(indexmap! {
+            String::from("*.{cjs,js,mjs}") => vec!(PluginNode {
+              package_name: String::from("b"),
+              resolve_from: Rc::new(String::from("~")),
+            })
+          })
+          .build()
+          .unwrap();
+
+        assert_eq!(
+          PartialParcelConfig::merge(from, extend),
+          PartialParcelConfigBuilder::default()
+            .compressors(indexmap! {
+              String::from("*.js") => vec!(PluginNode {
+                package_name: String::from("a"),
+                resolve_from: Rc::new(String::from("/")),
+              }),
+              String::from("*.{cjs,js,mjs}") => vec!(PluginNode {
+                package_name: String::from("b"),
+                resolve_from: Rc::new(String::from("~")),
+              }),
+            })
+            .build()
+            .unwrap()
+        );
+      }
+
+      #[test]
+      fn merges_pipelines_with_missing_dot_dot_dot() {
+        let from = PartialParcelConfigBuilder::default()
+          .compressors(indexmap! {
+            String::from("*.js") => vec!(PluginNode {
+              package_name: String::from("a"),
+              resolve_from: Rc::new(String::from("/")),
+            }, PluginNode {
+              package_name: String::from("b"),
+              resolve_from: Rc::new(String::from("/")),
+            })
+          })
+          .build()
+          .unwrap();
+
+        let extend = PartialParcelConfigBuilder::default()
+          .compressors(indexmap! {
+            String::from("*.js") => vec!(PluginNode {
+              package_name: String::from("c"),
+              resolve_from: Rc::new(String::from("/")),
+            })
+          })
+          .build()
+          .unwrap();
+
+        let expected = from.clone();
+
+        assert_eq!(PartialParcelConfig::merge(from, extend), expected);
+      }
+
+      #[test]
+      fn merges_pipelines_with_dot_dot_dot() {
+        let from = PartialParcelConfigBuilder::default()
+          .compressors(indexmap! {
+            String::from("*.js") => vec!(PluginNode {
+              package_name: String::from("a"),
+              resolve_from: Rc::new(String::from("/")),
+            },
+            PluginNode {
+              package_name: String::from("..."),
+              resolve_from: Rc::new(String::from("/")),
+            },
+            PluginNode {
+              package_name: String::from("c"),
+              resolve_from: Rc::new(String::from("/")),
+            })
+          })
+          .build()
+          .unwrap();
+
+        let extend = PartialParcelConfigBuilder::default()
+          .compressors(indexmap! {
+            String::from("*.js") => vec!(PluginNode {
+              package_name: String::from("b"),
+              resolve_from: Rc::new(String::from("~")),
+            })
+          })
+          .build()
+          .unwrap();
+
+        assert_eq!(
+          PartialParcelConfig::merge(from, extend),
+          PartialParcelConfigBuilder::default()
+            .compressors(indexmap! {
+              String::from("*.js") => vec!(PluginNode {
+                package_name: String::from("a"),
+                resolve_from: Rc::new(String::from("/")),
+              },
+              PluginNode {
+                package_name: String::from("b"),
+                resolve_from: Rc::new(String::from("~")),
+              },
+              PluginNode {
+                package_name: String::from("c"),
+                resolve_from: Rc::new(String::from("/")),
+              })
+            })
+            .build()
+            .unwrap()
+        );
+      }
+
+      #[test]
+      fn merges_pipelines_with_dot_dot_dot_match_in_grandparent() {
+        let from = PartialParcelConfigBuilder::default()
+          .compressors(indexmap! {
+            String::from("*.js") => vec!(PluginNode {
+              package_name: String::from("a"),
+              resolve_from: Rc::new(String::from("/")),
+            },
+            PluginNode {
+              package_name: String::from("..."),
+              resolve_from: Rc::new(String::from("/")),
+            },
+            PluginNode {
+              package_name: String::from("c"),
+              resolve_from: Rc::new(String::from("/")),
+            })
+          })
+          .build()
+          .unwrap();
+
+        let extend_1 = PartialParcelConfig::default();
+        let extend_2 = PartialParcelConfigBuilder::default()
+          .compressors(indexmap! {
+            String::from("*.js") => vec!(PluginNode {
+              package_name: String::from("b"),
+              resolve_from: Rc::new(String::from("~")),
+            })
+          })
+          .build()
+          .unwrap();
+
+        assert_eq!(
+          PartialParcelConfig::merge(PartialParcelConfig::merge(from, extend_1), extend_2),
+          PartialParcelConfigBuilder::default()
+            .compressors(indexmap! {
+              String::from("*.js") => vec!(PluginNode {
+                package_name: String::from("a"),
+                resolve_from: Rc::new(String::from("/")),
+              },
+              PluginNode {
+                package_name: String::from("b"),
+                resolve_from: Rc::new(String::from("~")),
+              },
+              PluginNode {
+                package_name: String::from("c"),
+                resolve_from: Rc::new(String::from("/")),
+              })
+            })
+            .build()
+            .unwrap()
+        );
+      }
     }
   }
 }
